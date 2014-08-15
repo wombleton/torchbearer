c31f3121064daaef5a2beb9755b908e9bae2fafa
Rowan Crawford <wombleton@gmail.com>
2014-08-15 17:34:47 +1200

    Only browserify after changes to assets

--- Gulpfile.js
+++ Gulpfile.js
@@ -14,12 +14,23 @@ gulp.task('browserify', function() {
   .pipe(gulp.dest('./public/js'));
 });
 
+function nodemon() {
+  plugins.nodemon({ script: 'index.js', ext: 'js', ignore: [ 'assets/**/*', 'public/**/*' ] })
+  .on('change', ['lint'])
+  .on('restart', function () {
+    console.log('restarted!');
+  });
+}
+
+function watch() {
+  plugins.watch({glob: 'assets/js/**/*.js'}, function() {
+    gulp.start('browserify');
+  });
+}
+
 gulp.task('develop', function() {
-  plugins.nodemon({ script: 'index.js', ext: 'html js', ignore: [] })
-    .on('change', ['lint', 'browserify'])
-    .on('restart', function () {
-      console.log('restarted!');
-    });
+  nodemon();
+  watch();
 });
 
 gulp.task('default', [ 'develop' ]);
