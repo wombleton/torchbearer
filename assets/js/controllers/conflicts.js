5c08c108155bddd53d8be211ed35f7b894d8d1bb
Rowan Crawford <wombleton@gmail.com>
2014-09-07 13:41:25 +1200

    Add a player list

--- assets/js/controllers/conflicts.js
+++ assets/js/controllers/conflicts.js
@@ -3,5 +3,45 @@ var controllers = require('./index');
 module.exports = controllers.controller('ConflictCtrl', [
   '$scope',
   function($scope) {
+    // start faked player data
+    $scope.players = [
+      {
+        name: 'Nale',
+        moves: [
+          {
+            name: 'Attack',
+            skill: 'Fighter',
+            level: 4
+          },
+          {
+            name: 'Defend',
+            skill: 'Health',
+            level: 6
+          },
+          {
+            name: 'Feight',
+            skill: 'Fighter',
+            level: 4
+          },
+          {
+            name: 'Manuever',
+            skill: 'Health',
+            level: 6
+          }
+        ],
+        weapon: {
+          name: 'Warhammer',
+          mods: {
+            A: {
+              dice: 1
+            },
+            D: {
+              dice: -1
+            }
+          }
+        }
+      }
+    ];
+    // end faked player data
   }
 ]);
